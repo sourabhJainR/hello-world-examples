30184d9ef0a7e8685482b43bf98d876e6d86197b
Sourabh Jain <sourjain2002@gmail.com>
2026-04-30 02:54:15 +0530

    Add hello_world_streamlit.py for a simple Streamlit application

---
+++ hello_world_streamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hello World", layout="centered")
+
+st.title("Hello, World!")
+st.write("This is a simple Streamlit application")
+
+st.info("Streamlit makes it easy to create data science and machine learning web apps in Python!")
