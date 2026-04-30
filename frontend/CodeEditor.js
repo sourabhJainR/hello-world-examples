7676355dd63587b38ab1ea2b52ce9329c2c20d24
Sourabh Jain <sourjain2002@gmail.com>
2026-04-30 08:06:37 +0530

    Add CodeEditor component

---
+++ frontend/CodeEditor.js
@@ -0,0 +1,11 @@
+// CodeEditor.js
+import React from 'react';
+
+const CodeEditor = () => {
+    return (
+        <div className="code-editor">
+            {/* Code Editor Implementation */}
+        </div>
+    );
+};
+export default CodeEditor;
